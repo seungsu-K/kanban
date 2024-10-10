5c188f2458224afcf094348c153a10cc12dacaa5
seungsu-K <kss095@naver.com>
2024-10-10 18:57:00 +0900

    [feat] login 페이지
    - 마크업 / 스타일링
    - email / pw 유효성 검증
    - input reset 버튼
    - 로그인 기능
    - user 정보 확인 시 페이지 이동

--- main.js
+++ main.js
@@ -1,10 +1,75 @@
 import '/style.css';
 import '@/layout/header.js';
-import '@/layout/navigation.js';
-import '@/layout/nav.js';
-import { getRecord } from './src/api/getRecord';
+import pb from './src/api/pocketbase';
+import { deleteButtonOn, resetInput } from './src/lib/deleteButton';
+import { getNode } from './src/lib/getNode';
+import { emailValidation, pwValidation } from './src/lib/validation';
 
-(async () => {
-  const a = await getRecord('users');
-  console.log(a);
-})();
+if (pb.authStore.isValid) {
+  location.href = '/src/pages/kanban/kanban.html';
+};
+
+
+// email 이벤트
+const userEmail = getNode('#userEmail');
+const emailDeleteButton = getNode('.button_delete_email');
+
+const handleEmail = (e) => {
+  const isEmailValid = emailValidation(e);
+  const errorMessage = getNode('#userEmailError');
+
+  if(isEmailValid) {
+    errorMessage.classList.remove('is-invalid');
+  } else {
+    errorMessage.classList.add('is-invalid');
+  }
+
+  deleteButtonOn(e);
+}
+
+userEmail.addEventListener('input', handleEmail);
+emailDeleteButton.addEventListener('click', resetInput);
+
+
+// password 이벤트
+const userPassword = getNode('#userPassword');
+const pwDeleteButton = getNode('.button_delete_password');
+
+const handlePassword = (e) => {
+  const isPwValid = pwValidation(e);
+  const errorMessage = getNode('#userPasswordError');
+
+  if(isPwValid) {
+    errorMessage.classList.remove('is-invalid');
+  } else {
+    errorMessage.classList.add('is-invalid');
+  }
+
+  deleteButtonOn(e);
+};
+
+userPassword.addEventListener('input', handlePassword);
+pwDeleteButton.addEventListener('click', resetInput);
+
+
+// login 이벤트
+const loginButton = getNode('#loginButton');
+
+const handleLogin = async (e) => {
+  e.preventDefault();
+
+  try {
+    const authData = await pb.collection('users').authWithPassword(userEmail.value, userPassword.value)
+    
+    location.reload();
+    // if(pb.authStore.isValid) {
+    //   location.href = '/src/pages/kanban/kanban.html';
+    // }
+  } catch (error) {
+    console.error('로그인 오류', error);
+    alert('로그인 중 오류가 발생했습니다. 다시 시도해주세요')
+  }
+};
+
+loginButton.addEventListener('click', handleLogin);
+
--- src/layout/nav.js
+++ src/layout/nav.js
@@ -1,11 +1,11 @@
-import { getNode } from '@/lib/getNode';
+// import { getNode } from '@/lib/getNode';
 
-const projectList = ['project1', 'project2', 'project3'];
+// const projectList = ['project1', 'project2', 'project3'];
 
-const projectLists = getNode('.projectLists');
+// const projectLists = getNode('.projectLists');
 
-const renderList = () => {
-  projectList.map((item) => projectLists.insertAdjacentHTML('beforeend', item));
-};
+// const renderList = () => {
+//   projectList.map((item) => projectLists.insertAdjacentHTML('beforeend', item));
+// };
 
-renderList();
+// renderList();
