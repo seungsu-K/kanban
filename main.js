f5fc07b250927e643d71613f710a8f48b7940e45
seungsu-K <kss095@naver.com>
2024-10-10 10:55:03 +0900

    [docs] getDocument -> getRecord

--- main.js
+++ main.js
@@ -2,9 +2,9 @@ import '/style.css';
 import '@/layout/header.js';
 import '@/layout/navigation.js';
 import '@/layout/nav.js';
-import { getRecord } from './src/api/getDocument';
+import { getRecord } from './src/api/getRecord';
 
 (async () => {
-  const a = await getRecord('projects');
+  const a = await getRecord('users');
   console.log(a);
 })();
